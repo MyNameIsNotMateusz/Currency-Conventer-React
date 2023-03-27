d502923e119001005edab3b10b4f6c83300c9016
Mateusz Otorowski <mati1otorowski@gmail.com>
2023-03-27 09:22:16 +0200

    i added currency time

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./Header";
 import Form from "./Form";
 import Result from "./Result";
@@ -6,11 +6,23 @@ import Result from "./Result";
 function App() {
   const [result, setResult] = useState(null);
   const [showResult, setShowResult] = useState(false);
+  const [date, setDate] = useState(new Date());
 
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+  
+    return () => clearInterval(timer);
+  }, []);
+  
   return (
     <>
-      <Header />
-      <Form setResult={setResult} setShowResult={setShowResult} />
+      <Header 
+      date={date.toDateString()}
+      />
+      <Form setResult={setResult} 
+      setShowResult={setShowResult} />
       {showResult && <Result result={result} />}
     </>
   );
--- src/Header/index.js
+++ src/Header/index.js
@@ -3,11 +3,13 @@ import "./style.css";
 const Header = (props) => {
     return (
         <div className="header">
-        <h1>Currency Conventer</h1>
-        <p className="header__item--secondHeading">Check foreing currency exchange rates</p>
+            <h1>Currency Converter</h1>
+            <p className="header__item--secondHeading">
+                Check foreign currency exchange rates
+            </p>
+            <p className="header__item--time">Current Time: {props.date}</p>
         </div>
-    )
+    );
 };
-
 
 export default Header;
