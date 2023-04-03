fc27e0d1944a1e54bc8fbefffacbc536b741d946
Mateusz Otorowski <mati1otorowski@gmail.com>
2023-04-03 12:40:28 +0200

    i added new custom hook to date

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Header from "./Header";
 import Form from "./Form";
 import Result from "./Result";
+import { useCurrentDate } from "./useCurrentDate";
 
 function App() {
   const [result, setResult] = useState(null);
   const [showResult, setShowResult] = useState(false);
-  const [date, setDate] = useState(new Date());
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setDate(new Date());
-    }, 1000);
-  
-    return () => clearInterval(timer);
-  }, []);
+  const { date, setDate, } = useCurrentDate();
   
   return (
     <>
