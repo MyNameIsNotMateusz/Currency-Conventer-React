53fe87d58da3496fedf97832bef51d66a81d4fe1
Mateusz Otorowski <mati1otorowski@gmail.com>
2023-04-09 16:45:15 +0200

    I added current exchange rates from the API

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,59 +1,58 @@
 import { useState } from "react";
+import axios from "axios";
 import { FormWrapper, LabelWrapper, Input, Button } from "./styled";
 
 const Form = ({ setResult, setShowResult }) => {
-  const [amount, setAmount] = useState(0);
-  const [fromCurrency, setFromCurrency] = useState("USD");
-  const [toCurrency, setToCurrency] = useState("EUR");
 
-  const convertCurrency = (event) => {
+  const [conversionData, setConversionData] = useState(null);
+
+  const handleFormSubmit = async (event) => {
     event.preventDefault();
-    const rates = {
-      USD: 1,
-      EUR: 0.83,
-      GBP: 0.72,
-      PLN: 3.88
-    };
-
-    const exchangeRate = rates[toCurrency] / rates[fromCurrency];
-    setResult(amount * exchangeRate);
-    setShowResult(true);
+  
+    const amount = document.getElementById("amount").value;
+    const fromCurrency = document.getElementById("fromCurrency").value;
+    const toCurrency = document.getElementById("toCurrency").value;
+  
+    try {
+      const response = await axios.get(`https://api.exchangerate.host/convert?from=${fromCurrency}&to=${toCurrency}&amount=${amount}`);
+      const result = response.data.result;
+      setResult(result);
+      setShowResult(true);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
     <>
       <FormWrapper
-        className="form"
-        onSubmit={convertCurrency}>
+      onSubmit={handleFormSubmit}
+        className="form">
         <LabelWrapper>Amount</LabelWrapper>
         <Input
           id="amount"
           type="number"
           step="0.01"
-          value={amount}
-          onChange={(event) => setAmount(event.target.value)}
         />
         <label className="form__item--label">From:</label>
         <Input as="select"
           id="fromCurrency"
-          value={fromCurrency}
-          onChange={(event) => setFromCurrency(event.target.value)}
         >
           <option value="USD">USD - US Dollar</option>
           <option value="EUR">EUR - Euro</option>
           <option value="GBP">GBP - British Pound</option>
           <option value="PLN">PLN - Polish Zloty</option>
+          <option value="NOK">NOK - Norwegian Kroner</option>
         </Input>
         <label className="form__item--label">To:</label>
         <Input as="select"
           id="toCurrency"
-          value={toCurrency}
-          onChange={(event) => setToCurrency(event.target.value)}
         >
           <option value="USD">USD - US Dollar</option>
           <option value="EUR">EUR - Euro</option>
           <option value="GBP">GBP - British Pound</option>
           <option value="PLN">PLN - Polish Zloty</option>
+          <option value="NOK">NOK - Norwegian Kroner</option>
         </Input>
         <Button type="submit">See the result</Button>
       </FormWrapper>
